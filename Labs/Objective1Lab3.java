f6c69779db8cb2733d14391dde48dda47be7a0cf
Julian <juliangoudy2@gmail.com>
2018-02-27 21:19:36 -0700

    completed objective1lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3{
+  public static void main(String[] args){
+
+    System.out.println("Name : Julian Goudy");
+    System.out.println("Age : 25");
+    System.out.println("State : Michigan");
+
+  }
+}
