f352b0bdd34c7117aa9461d1948bbc0109b29805
Julian <juliangoudy2@gmail.com>
2018-03-04 00:35:06 -0700

    completed Obj2Lab5

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,11 @@
+public class Objective2Lab5 {
+  public static void main(String[] args) {
+    double side1 = 5, side2 = 8, num, hypotenuse;
+
+    num = (side1*side1) + (side2*side2);
+    hypotenuse = Math.sqrt(num);
+    System.out.println(hypotenuse);
+
+
+  }
+}
