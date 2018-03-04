8f416ea40f77d78fdf27637da047ec2aed0b6801
Julian <juliangoudy2@gmail.com>
2018-03-03 23:52:12 -0700

    completed ObjLab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Julian Goudy";
+    int age = 25;
+    String state = "Michigan";
+
+    System.out.println("Name: " + name + "Age: " + age + "State: " + state);
+
+  }
+}
