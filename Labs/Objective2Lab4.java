2ec4ff27db63d8b55f51fe3b0719413233b75b28
Julian <juliangoudy2@gmail.com>
2018-03-04 00:16:36 -0700

    completed Obj2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,10 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1 = 2, num2 = 6, num3 = 8, average;
+
+    average = num1+num2+num3/3;
+    System.out.println(average);
+
+
+  }
+}
