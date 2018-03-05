ddd0a2d1251a212adb775c25f5c76857bf21e24d
Julian <juliangoudy2@gmail.com>
2018-03-04 17:56:40 -0700

    complete Obj6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,11 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int i = 1, total=0;
+
+    while (i <= 20) {
+           total += i;
+           i++;
+    }
+    System.out.println(total);
+  }
+}
